e1a591dd82d8ccd0131700c6458a68f8be8304c0
Lindroy <linyulong2014@163.com>
2017-04-15 18:05:12 +0800

    创建项目

---
+++ app/src/main/java/com/lindroid/tablayoutdemo/TabFragment.java
@@ -0,0 +1,38 @@
+package com.lindroid.tablayoutdemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by admin on 2017/4/15.
+ */
+
+public class TabFragment extends Fragment {
+    private Context context;
+    private String content;
+    public TabFragment() {
+
+    }
+
+    public TabFragment(Context context,String content){
+        this.context = context;
+        this.content = content;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(context);
+        textView.setText(content);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+}
